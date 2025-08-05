c316508709b78dc24feb44bac59117d8c424c22b
Prithika <91656490+Prithika496@users.noreply.github.com>
2025-08-05 20:16:01 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SymptomForm from "./components/SymptomForm";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>🧠 AI Medical Diagnosis Expert System</h1>
+      <SymptomForm />
+    </div>
+  );
+};
+
+export default App;
